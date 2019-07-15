ad9698050ae8a0882ce79e2a44379fb59c6c2d4b
cixmr <cixmr@DESKTOP-F6FH1EO>
2019-07-15 00:44:48 -0600

    agregando link

--- QuieroPizza/QuieroPizza.Web/Controllers/ProductosController.cs
+++ QuieroPizza/QuieroPizza.Web/Controllers/ProductosController.cs
@@ -17,11 +17,11 @@ namespace QuieroPizza.Web.Controllers
             producto1.Descripcion = " Pizza 6 Quesos";
 
             var producto2 = new ProductoModel();
-            producto2.Id = 1;
+            producto2.Id = 2;
             producto2.Descripcion = " Pizza 4 Estaciones";
 
             var producto3 = new ProductoModel();
-            producto3.Id = 1;
+            producto3.Id = 3;
             producto3.Descripcion = " Pizza Artesanal";
 
             var listadeProductos = new List<ProductoModel>();
